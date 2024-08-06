ea664840e09addfc92e22735602543db790d0cb0
carlyMhayter <carlyhayter@gmail.com>
2024-08-06 11:26:00 -0700

    amend dto for status won

--- src/monday/dto/status-won.dto.ts
+++ src/monday/dto/status-won.dto.ts
@@ -1,31 +1,35 @@
 export class PostStatusToWonDto {
-  userId: number;
-  originalTriggerUuid: any;
-  boardId: number;
-  groupId: string;
-  pulseId: number;
-  pulseName: string;
-  columnId: string;
-  columnType: string;
-  columnTitle: string;
-  value: {
-    label: {
-      index: 3;
-      text: string;
-      style: {
-        color: string;
-        border: string;
-        var_name: string;
-      };
-    };
-    post_id: any;
-  };
-  previousValue: any;
-  changedAt: any;
-  isTopGroup: boolean;
-  app: string;
-  type: string;
-  triggerTime: string;
-  subscriptionId: number;
-  triggerUuid: string;
+  event: any;
 }
+
+// export class PostStatusToWonDto {
+//   userId: number;
+//   originalTriggerUuid: any;
+//   boardId: number;
+//   groupId: string;
+//   pulseId: number;
+//   pulseName: string;
+//   columnId: string;
+//   columnType: string;
+//   columnTitle: string;
+//   value: {
+//     label: {
+//       index: 3;
+//       text: string;
+//       style: {
+//         color: string;
+//         border: string;
+//         var_name: string;
+//       };
+//     };
+//     post_id: any;
+//   };
+//   previousValue: any;
+//   changedAt: any;
+//   isTopGroup: boolean;
+//   app: string;
+//   type: string;
+//   triggerTime: string;
+//   subscriptionId: number;
+//   triggerUuid: string;
+// }
