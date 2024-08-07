a17f771a4eadfe61cf30d5cf489507e437637495
carlyMhayter <carlyhayter@gmail.com>
2024-08-07 09:21:26 -0700

    test with axios request

--- src/monday/monday.controller.ts
+++ src/monday/monday.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Get, Param, Query, Body, Req } from '@nestjs/common';
 import * as rawbody from 'raw-body';
 import * as process from 'process';
-
+import axios from 'axios';
 @Controller('monday')
 export class MondayController {
   @Get()
@@ -29,34 +29,81 @@ export class MondayController {
       if (!!data.event) {
         console.log('data:', data);
 
-        const myHeaders = new Headers();
-        myHeaders.append('Content-Type', 'application/json');
-        myHeaders.append('Authorization', process.env.MONDAY_AUTH);
-        myHeaders.append(
-          'Cookie',
-          '__cf_bm=m8zc61.IT0xf6oKWKbBo0QWuPhgxPFUC1dW87JwdnpE-1723044832-1.0.1.1-jUOMtZtUcNVOa.AdRWzi6gzMAurMpB6iDfAZol1F8eKTorhtD5fLHGey_bZSPocyVGvoqr2OMshqhyFugxndrzYrXfWvJml80MJlgJOvxY8',
-        );
+        // const myHeaders = new Headers();
+        // myHeaders.append('Content-Type', 'application/json');
+        // myHeaders.append('Authorization', process.env.MONDAY_AUTH);
+        // myHeaders.append(
+        //   'Cookie',
+        //   '__cf_bm=m8zc61.IT0xf6oKWKbBo0QWuPhgxPFUC1dW87JwdnpE-1723044832-1.0.1.1-jUOMtZtUcNVOa.AdRWzi6gzMAurMpB6iDfAZol1F8eKTorhtD5fLHGey_bZSPocyVGvoqr2OMshqhyFugxndrzYrXfWvJml80MJlgJOvxY8',
+        // );
 
-        const graphql = JSON.stringify({
-          query:
-            'mutation{\n create_board(  \ntemplate_id: 6198096739\n  board_name: "Testing"\ndescription: "board created as test for automation"\nboard_kind: public\nfolder_id: 7860571\nworkspace_id: 1080416\nboard_owner_ids: [37385671]\nboard_owner_team_ids: [614284]\nboard_subscriber_ids: [37385671]\nboard_subscriber_teams_ids: [614284]\nempty: false\n){id}\n\n}',
+        // const graphql = JSON.stringify({
+        //   query:
+        //     'mutation{\n create_board(  \ntemplate_id: 6198096739\n  board_name: "Testing"\ndescription: "board created as test for automation"\nboard_kind: public\nfolder_id: 7860571\nworkspace_id: 1080416\nboard_owner_ids: [37385671]\nboard_owner_team_ids: [614284]\nboard_subscriber_ids: [37385671]\nboard_subscriber_teams_ids: [614284]\nempty: false\n){id}\n\n}',
+        // });
+        // const requestOptions = {
+        //   method: 'POST',
+        //   headers: myHeaders,
+        //   body: graphql,
+        //   // redirect: 'follow',
+        // };
+        // console.log('requestOptions', requestOptions);
+        // fetch('https://api.monday.com/v2', requestOptions)
+        //   .then((response) =>
+        //     console.log(console.log('response.text()', response.text())),
+        //   )
+        //   .then((result) => console.log('result ', result))
+        //   .catch((error) => console.error('error', error));
+
+        const axios = require('axios');
+        let jsondata = JSON.stringify({
+          query: `mutation{
+ create_board(  
+template_id: 6198096739
+  board_name: "Testing"
+description: "board created as test for automation"
+board_kind: public
+folder_id: 7860571
+workspace_id: 1080416
+board_owner_ids: [37385671]
+board_owner_team_ids: [614284]
+board_subscriber_ids: [37385671]
+board_subscriber_teams_ids: [614284]
+empty: false
+){id}
+
+}`,
           variables: {},
         });
-        const requestOptions = {
-          method: 'POST',
-          headers: myHeaders,
-          body: graphql,
-          // redirect: 'follow',
-        };
-        console.log('requestOptions', requestOptions);
-        fetch('https://api.monday.com/v2', requestOptions)
-          .then((response) =>
-            console.log(console.log('response.text()', response.text())),
-          )
-          .then((result) => console.log('result ', result))
-          .catch((error) => console.error('error', error));
 
-        //send post request to monday
+        let config = {
+          method: 'post',
+          maxBodyLength: Infinity,
+          url: 'https://api.monday.com/v2',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.MONDAY_AUTH}`,
+            Cookie:
+              '__cf_bm=m8zc61.IT0xf6oKWKbBo0QWuPhgxPFUC1dW87JwdnpE-1723044832-1.0.1.1-jUOMtZtUcNVOa.AdRWzi6gzMAurMpB6iDfAZol1F8eKTorhtD5fLHGey_bZSPocyVGvoqr2OMshqhyFugxndrzYrXfWvJml80MJlgJOvxY8',
+          },
+          data: jsondata,
+        };
+
+        console.log(
+          'config*************************************************************************',
+          config,
+        );
+        axios
+          .request(config)
+          .then((response) => {
+            console.log('*********************************response');
+            console.log(JSON.stringify(response.data));
+          })
+          .catch((error) => {
+            console.log('*********************************error');
+
+            console.log(error);
+          });
       } else {
         //if there is not an event field on the body
         //it's the verification request
