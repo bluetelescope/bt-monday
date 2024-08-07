4671e803428130a7f088d83ee4c6e3f6a8d5d1c2
carlyMhayter <carlyhayter@gmail.com>
2024-08-07 09:41:34 -0700

    amend graphql request

--- src/monday/monday.controller.ts
+++ src/monday/monday.controller.ts
@@ -56,6 +56,11 @@ export class MondayController {
         //   .catch((error) => console.error('error', error));
 
         const axios = require('axios');
+
+        const graphql = JSON.stringify({
+          query:
+            'mutation{\n create_board(  \ntemplate_id: 6198096739\n  board_name: "Testing"\ndescription: "board created as test for automation"\nboard_kind: public\nfolder_id: 7860571\nworkspace_id: 1080416\nboard_owner_ids: [37385671]\nboard_owner_team_ids: [614284]\nboard_subscriber_ids: [37385671]\nboard_subscriber_teams_ids: [614284]\nempty: false\n){id}\n\n}',
+        });
         let jsondata = JSON.stringify({
           query: `mutation{
  create_board(  
@@ -86,7 +91,7 @@ empty: false
             Cookie:
               '__cf_bm=m8zc61.IT0xf6oKWKbBo0QWuPhgxPFUC1dW87JwdnpE-1723044832-1.0.1.1-jUOMtZtUcNVOa.AdRWzi6gzMAurMpB6iDfAZol1F8eKTorhtD5fLHGey_bZSPocyVGvoqr2OMshqhyFugxndrzYrXfWvJml80MJlgJOvxY8',
           },
-          data: jsondata,
+          data: graphql,
         };
 
         console.log(
@@ -100,7 +105,7 @@ empty: false
             console.log(JSON.stringify(response.data));
           })
           .catch((error) => {
-            console.log('*********************************error');
+            console.log('*********************************response');
 
             console.log(error);
           });
