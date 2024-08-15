196fd618de63cdc34de753bafdcaadf72f6f5a82
carlyMhayter <carlyhayter@gmail.com>
2024-08-15 16:42:07 -0700

    update more

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TimetrackingModule } from './timetracking/timetracking.module';
+import { PopulateModule } from './populate/populate.module';
 import { AppService } from './app.service';
 import { MondayModule } from './monday/monday.module';
 import { MondayItemModule } from './monday-item/monday-item.module';
 
 @Module({
-  imports: [MondayModule, MondayItemModule, TimetrackingModule],
+  imports: [MondayModule, MondayItemModule, TimetrackingModule, PopulateModule],
   controllers: [AppController],
   providers: [AppService],
 })
