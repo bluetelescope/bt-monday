6f5490dd388315666cbfde4874e16ecb237f9172
carlyMhayter <carlyhayter@gmail.com>
2024-08-29 10:40:18 -0700

    intialize request to active board from loas

--- src/loas/loas.controller.ts
+++ src/loas/loas.controller.ts
@@ -110,10 +110,93 @@ export class LOASController {
               (column) => column.id === 'board_relation',
             )[0].value;
             boardRelation = JSON.parse(boardRelationValue);
+            boardId = boardRelation.linkedPulseIds[0].linkedPulseId;
+
             console.log('recipientName', recipientName);
             console.log('cost', cost);
             console.log('boardRelationValue', boardRelationValue);
             console.log('boardRelation', boardRelation);
+            console.log('boardId', boardId);
+
+            const getBoardItemQuery = returnGetItemFromBoard(
+              boardId,
+              'name',
+              'LOAs',
+            );
+            console.log('getBoardItemQuery', getBoardItemQuery);
+            const getBoardItemCofig = returnGetConfig(getBoardItemQuery);
+            return axios.request(getBoardItemCofig);
+          })
+          .then((getBoardItemsRes) => {
+            console.log(
+              'getBoardItemsRes *****************************************************************',
+            );
+            console.log(
+              'getBoardItemsRes.data.data',
+              getBoardItemsRes.data.data,
+            );
+
+            itemIDinBoard =
+              getBoardItemsRes.data.data.boards[0].items_page.items[0].id;
+
+            //GET: columns in subitem
+            const getItemColumnsQuery = returnColumnsInSubitem(itemIDinBoard);
+            const getItemColumnsConfig = returnGetConfig(getItemColumnsQuery);
+            return axios.request(getItemColumnsConfig);
+          })
+          .then((getItemColumnsRes) => {
+            console.log(
+              'getItemColumnsRes *****************************************************************',
+            );
+            //parse columns data
+            const columns =
+              getItemColumnsRes.data.data.items[0].subitems[0].column_values;
+            console.log('columns', columns);
+
+            subitemRateColumnId = parseSubColumnValuesForString(
+              columns,
+              'Rate',
+            );
+            subitemHoursColumnId = parseSubColumnValuesForString(
+              columns,
+              'Hours',
+            );
+
+            console.log('subitemRateColumnId', subitemRateColumnId);
+            console.log('subitemHoursColumnId', subitemHoursColumnId);
+
+            //TODO: replace getting the item and replacing the entries with create new subitem
+            let postSubitemQuery = `mutation ($columnVals: JSON!,) { create_subitem(parent_item_id: ${itemIDinBoard},item_name: "Hours Log",create_labels_if_missing: true, column_values:$columnVals) { id } }`;
+            let testing = {
+              name: `${recipientName} - ID:${itemId} `,
+              person: {
+                personsAndTeams: [{ id: personId, kind: 'person' }],
+              },
+            };
+
+            testing[`${subitemRateColumnId}`] = 1;
+            testing[`${subitemHoursColumnId}`] = cost;
+
+            console.log('testing', testing);
+
+            let vars = {
+              columnVals: JSON.stringify(testing),
+            };
+            console.log('vars', vars);
+
+            //POST: new subitem
+            const postSubitemConfig = postConfigWithVariables(
+              postSubitemQuery,
+              vars,
+            );
+            console.log('postSubitemConfig', postSubitemConfig);
+            return axios.request(postSubitemConfig);
+          })
+          .then((postSubitemRes) => {
+            console.log(
+              'postCostToColumnRes**********************************************************************',
+              postSubitemRes.data,
+            );
           })
           .catch((error) => {
             console.log('error.data', error.data);
