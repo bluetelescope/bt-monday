5398b03a1c3c34e54e8a6d4ae69e5397d8c2dc2a
carlyMhayter <carlyhayter@gmail.com>
2024-10-29 10:17:17 -0700

    remove cookie from get request config

--- src/functions/returnConfig.ts
+++ src/functions/returnConfig.ts
@@ -6,8 +6,8 @@ export function returnGetConfig(graphqlGet: any) {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${process.env.MONDAY_AUTH}`,
-      Cookie:
-        '__cf_bm=m8zc61.IT0xf6oKWKbBo0QWuPhgxPFUC1dW87JwdnpE-1723044832-1.0.1.1-jUOMtZtUcNVOa.AdRWzi6gzMAurMpB6iDfAZol1F8eKTorhtD5fLHGey_bZSPocyVGvoqr2OMshqhyFugxndrzYrXfWvJml80MJlgJOvxY8',
+      // Cookie:
+      //   '__cf_bm=m8zc61.IT0xf6oKWKbBo0QWuPhgxPFUC1dW87JwdnpE-1723044832-1.0.1.1-jUOMtZtUcNVOa.AdRWzi6gzMAurMpB6iDfAZol1F8eKTorhtD5fLHGey_bZSPocyVGvoqr2OMshqhyFugxndrzYrXfWvJml80MJlgJOvxY8',
     },
     data: graphqlGet,
   };
