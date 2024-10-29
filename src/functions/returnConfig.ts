7ff83ef576d779289b3e108732cae9e705f09adb
carlyMhayter <carlyhayter@gmail.com>
2024-10-29 10:22:00 -0700

    remove all cookies from all query config

--- src/functions/returnConfig.ts
+++ src/functions/returnConfig.ts
@@ -21,8 +21,8 @@ export function returnPostConfig(graphqlPost: any) {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${process.env.MONDAY_AUTH}`,
-      Cookie:
-        '__cf_bm=m8zc61.IT0xf6oKWKbBo0QWuPhgxPFUC1dW87JwdnpE-1723044832-1.0.1.1-jUOMtZtUcNVOa.AdRWzi6gzMAurMpB6iDfAZol1F8eKTorhtD5fLHGey_bZSPocyVGvoqr2OMshqhyFugxndrzYrXfWvJml80MJlgJOvxY8',
+      // Cookie:
+      //   '__cf_bm=m8zc61.IT0xf6oKWKbBo0QWuPhgxPFUC1dW87JwdnpE-1723044832-1.0.1.1-jUOMtZtUcNVOa.AdRWzi6gzMAurMpB6iDfAZol1F8eKTorhtD5fLHGey_bZSPocyVGvoqr2OMshqhyFugxndrzYrXfWvJml80MJlgJOvxY8',
     },
     data: graphqlPost,
   };
@@ -36,8 +36,8 @@ export function postConfigWithVariables(testBody: any, vars: any) {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${process.env.MONDAY_AUTH}`,
-      Cookie:
-        '__cf_bm=m8zc61.IT0xf6oKWKbBo0QWuPhgxPFUC1dW87JwdnpE-1723044832-1.0.1.1-jUOMtZtUcNVOa.AdRWzi6gzMAurMpB6iDfAZol1F8eKTorhtD5fLHGey_bZSPocyVGvoqr2OMshqhyFugxndrzYrXfWvJml80MJlgJOvxY8',
+      // Cookie:
+      //   '__cf_bm=m8zc61.IT0xf6oKWKbBo0QWuPhgxPFUC1dW87JwdnpE-1723044832-1.0.1.1-jUOMtZtUcNVOa.AdRWzi6gzMAurMpB6iDfAZol1F8eKTorhtD5fLHGey_bZSPocyVGvoqr2OMshqhyFugxndrzYrXfWvJml80MJlgJOvxY8',
     },
     data: { query: testBody, variables: vars },
   };
